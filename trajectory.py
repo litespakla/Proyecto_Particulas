a6b30c2c7dcff68986fb5d5f59ad7de2a5f6e217
witowin <73556258+witowin@users.noreply.github.com>
2020-11-16 17:21:14 -0600

    Update trajectory.py

--- trajectory.py
+++ trajectory.py
@@ -8,9 +8,9 @@ import math
 
 #Set world size in mm inside the float parentheses
 
-xWorld= float(500.00)*100
-yWorld= float(500.00)*100
-zWorld= float(500.00)*100
+xWorld= float(230.00)*100/2
+yWorld= float(230.00)*100/2
+zWorld= float(230.00)*100/2
 
 
 #Start Point
@@ -20,9 +20,9 @@ z0= float(sys.argv[3])*100
 
 
 #Momentum
-px= float(sys.argv[4])*100
-py= float(sys.argv[5])*100
-pz= float(sys.argv[6])*100
+px= float(sys.argv[4])
+py= float(sys.argv[5])
+pz= float(sys.argv[6])
 
 
 #Actual Volume Definition
@@ -30,39 +30,44 @@ pz= float(sys.argv[6])*100
 def getVolume(x, y, z):
 
 #Set Boundaries
-  if(x <0 or y <0 or z <0 or x >xWorld or y >yWorld or z >zWorld):
+  if(x < -xWorld or y < -yWorld or z < -zWorld or x > xWorld or y > yWorld or z > zWorld):
     return "Exit"
-    
+
+#Define lenght of arm
+  
+  if( z > 105.00 *100 or z < -105.00 *100 ):
+    return "Air"
+  
 #Define Bone in mm before de *100 multiplier
   rb= 30.00 *100    #Bone radius
-  ybc= 250.00 *100  #Y cylinder center
-  zbc= 150.00 *100  #Z cylinder center
+  xbc= 0 *100  #X cylinder center
+  ybc= 0 *100  #Y cylinder center
 #Ask for Bone
-  if( pow( y-ybc ,2) + pow( z-zbc ,2) <= pow( rb ,2) ):
+  if( pow( x-xbc ,2) + pow( y-ybc ,2) <= pow( rb ,2) ):
     return "Bone"
 
 #Define Muscle in mm before de *100 multiplier
   rm= 100.00 *100   #Muscle radius
-  ymc= 250.00 *100  #Y muscle center
-  zmc= 150.00 *100  #Z muscle center
+  xmc= 0 *100  #X muscle center
+  ymc= 0 *100  #Y muscle center
 #Ask for Muscle
-  if( pow( y-ymc ,2) + pow( z-zmc ,2) <= pow( rm ,2) ):
+  if( pow( x-xmc ,2) + pow( y-ymc ,2) <= pow( rm ,2) ):
     return "Muscle"
 
 #Define Fat in mm before de *100 multiplier
   rf= 105.00 *100   #Fat radius
-  pyc= 250.00 *100  #Y fat center
-  pzc= 150.00 *100  #Z fat center
-#Ask for Skin
-  if( pow( y-pyc ,2) + pow( z-pzc ,2) <= pow( rf ,2) ):
+  xfc= 0 *100  #X fat center
+  yfc= 0 *100  #Y fat center
+#Ask for Fat
+  if( pow( x-xfc ,2) + pow( y-yfc ,2) <= pow( rf ,2) ):
     return "Fat"
 
 #Define Skin in mm before de *100 multiplier
   rs= 110.00 *100   #Skin radius
-  ysc= 250.00 *100  #Y skin center
-  zsc= 150.00 *100  #Z skin center
+  xsc= 0 *100  #X skin center
+  ysc= 0 *100  #Y skin center
 #Ask for Skin
-  if( pow( y-ysc ,2) + pow( z-zsc ,2) <= pow( rs ,2) ):
+  if( pow( x-xsc ,2) + pow( y-ysc ,2) <= pow( rs ,2) ):
     return "Skin"
 
 #Anything else is Air
@@ -87,14 +92,29 @@ yb= y0
 zb= z0
 volume= getVolume(xa, ya, za)
 
+#Set Distance and Points
+
+i= 0
+dis= [0,0,0,0,0,0,0,0,0]
+pto= [[x0,y0,z0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+     [0,0,0]]
+
 
 #Trayectory
 
-if pow(zt, 2) >= pow(xt, 2) + pow(yt, 2):
+if pow(xt, 2) >= pow(yt, 2) + pow(zt, 2):
 
-  if zt <= 0: #Z is desending
+  if xt <= 0: #X is desending
     stay= True
-    while zb>-1 and stay:
+    while xb>-xWorld-1 and stay:
       
       xb= xb + xt
       yb= yb + yt
@@ -102,14 +122,133 @@ if pow(zt, 2) >= pow(xt, 2) + pow(yt, 2):
       
       if volume != getVolume(xb, yb, zb):    
         r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
-        print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
+        #print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
+        
+        dis[i]= r
+        pto[i+1][0]= xb-xt 
+        pto[i+1][1]= yb-yt
+        pto[i+1][2]= zb-zt
+        i= i+1
+        
         xa= xb
         ya= yb
         za= zb
         volume= getVolume(xb, yb, zb)
         if volume == "Exit":
           stay= False
-          print(volume)
+          #print(volume)
+  
+  
+  else:  #X is asending
+    stay=True
+    while xb<xWorld+1 and stay:
+      
+      xb= xb + xt
+      yb= yb + yt
+      zb= zb + zt
+      
+      if volume != getVolume(xb, yb, zb):    
+        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
+        #print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
+        
+        dis[i]= r
+        pto[i+1][0]= xb-xt 
+        pto[i+1][1]= yb-yt
+        pto[i+1][2]= zb-zt
+        i= i+1
+        
+        xa= xb
+        ya= yb
+        za= zb
+        volume= getVolume(xb, yb, zb)
+        if volume == "Exit":
+          stay= False
+          #print(volume)
+
+
+elif pow(yt, 2) >= pow(xt, 2) + pow(zt, 2):
+
+  if yt <= 0: #Y is desending
+    stay= True
+    while yb>-yWorld-1 and stay:
+      
+      xb= xb + xt
+      yb= yb + yt
+      zb= zb + zt
+      
+      if volume != getVolume(xb, yb, zb):    
+        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
+        #print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
+        
+        dis[i]= r
+        pto[i+1][0]= xb-xt 
+        pto[i+1][1]= yb-yt
+        pto[i+1][2]= zb-zt
+        i= i+1
+        
+        xa= xb
+        ya= yb
+        za= zb
+        volume= getVolume(xb, yb, zb)
+        if volume == "Exit":
+          stay= False
+          #print(volume)
+  
+  
+  else:  #Y is asending
+    stay=True
+    while yb<yWorld+1 and stay:
+      
+      xb= xb + xt
+      yb= yb + yt
+      zb= zb + zt
+      
+      if volume != getVolume(xb, yb, zb):    
+        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
+        #print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
+        
+        dis[i]= r
+        pto[i+1][0]= xb-xt 
+        pto[i+1][1]= yb-yt
+        pto[i+1][2]= zb-zt
+        i= i+1
+        
+        xa= xb
+        ya= yb
+        za= zb
+        volume= getVolume(xb, yb, zb)
+        if volume == "Exit":
+          stay= False
+          #print(volume)
+   
+          
+else:
+
+  if zt <= 0: #Z is desending
+    stay= True
+    while zb>-zWorld-1 and stay:
+      
+      xb= xb + xt
+      yb= yb + yt
+      zb= zb + zt
+      
+      if volume != getVolume(xb, yb, zb):    
+        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
+        #print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
+        
+        dis[i]= r
+        pto[i+1][0]= xb-xt 
+        pto[i+1][1]= yb-yt
+        pto[i+1][2]= zb-zt
+        i= i+1
+        
+        xa= xb
+        ya= yb
+        za= zb
+        volume= getVolume(xb, yb, zb)
+        if volume == "Exit":
+          stay= False
+          #print(volume)
   
   
   else:  #Z is asending
@@ -122,93 +261,84 @@ if pow(zt, 2) >= pow(xt, 2) + pow(yt, 2):
       
       if volume != getVolume(xb, yb, zb):    
         r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
-        print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
-        xa= xb
-        ya= yb
-        za= zb
-        volume= getVolume(xb, yb, zb)
-        if volume == "Exit":
-          stay= False
-          print(volume)
+        #print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
         
-elif pow(xt, 2) >= pow(yt, 2) + pow(zt, 2):
+        dis[i]= r
+        pto[i+1][0]= xb-xt 
+        pto[i+1][1]= yb-yt
+        pto[i+1][2]= zb-zt
+        i= i+1
+        
+        xa= xb
+        ya= yb
+        za= zb
+        volume= getVolume(xb, yb, zb)
+        if volume == "Exit":
+          stay= False
+          #print(volume)
 
-  if xt <= 0: #X is desending
-    stay= True
-    while xb>-1 and stay:
-      
-      xb= xb + xt
-      yb= yb + yt
-      zb= zb + zt
-      
-      if volume != getVolume(xb, yb, zb):    
-        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
-        print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
-        xa= xb
-        ya= yb
-        za= zb
-        volume= getVolume(xb, yb, zb)
-        if volume == "Exit":
-          stay= False
-          print(volume)
+
+#Set return Variabels
+
+disf= [0,0,0,0,0,0,0]
+ptof= [[0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0]]
+
+
+#Arrange nubmer on return variables
+
+m=-1
+volume= getVolume(x0, y0, z0)
+
+if volume == "Skin" and i > 5:
+  m= 0
+
+elif volume == "Fat" and i > 5:
+  m= 1
+
+elif volume == "Muscle" and i > 5:
+  m= 2
+
+elif volume == "Bone" :
+  m= 3
+
+elif volume == "Muscle" and i < 5:
+  m= 4
+
+elif volume == "Fat" and i < 5:
+  m= 5
+
+elif volume == "Skin" and i < 5:
+  m= 6
+
+for x in range(0,i-1,1):
+
+  disf[x+m]= round((dis[x])/100, 2)
   
-  
-  else:  #X is asending
-    stay=True
-    while xb<zWorld+1 and stay:
-      
-      xb= xb + xt
-      yb= yb + yt
-      zb= zb + zt
-      
-      if volume != getVolume(xb, yb, zb):    
-        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
-        print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
-        xa= xb
-        ya= yb
-        za= zb
-        volume= getVolume(xb, yb, zb)
-        if volume == "Exit":
-          stay= False
-          print(volume)
+  ptof[x+m+1][0]= round((pto[x+1][0])/100, 2)
+  ptof[x+m+1][1]= round((pto[x+1][1])/100, 2)
+  ptof[x+m+1][2]= round((pto[x+1][2])/100, 2)
+
+
+if volume == "Air":
+  ptof[0][0]= round( pto[1][0]/100 ,2) 
+  ptof[0][1]= round( pto[1][1]/100 ,2)
+  ptof[0][2]= round( pto[1][2]/100 ,2)
 
 else:
 
-  if yt <= 0: #Y is desending
-    stay= True
-    while yb>-1 and stay:
-      
-      xb= xb + xt
-      yb= yb + yt
-      zb= zb + zt
-      
-      if volume != getVolume(xb, yb, zb):    
-        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
-        print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
-        xa= xb
-        ya= yb
-        za= zb
-        volume= getVolume(xb, yb, zb)
-        if volume == "Exit":
-          stay= False
-          print(volume)
-  
-  
-  else:  #Y is asending
-    stay=True
-    while yb<zWorld+1 and stay:
-      
-      xb= xb + xt
-      yb= yb + yt
-      zb= zb + zt
-      
-      if volume != getVolume(xb, yb, zb):    
-        r= math.sqrt( pow(xb-xa ,2) + pow(yb-ya ,2) + pow(zb-za ,2))
-        print(round(r/100, 2), "\t mm in ", volume, " at:\t", round((xb-xt)/100, 2), round((yb-yt)/100, 2), round((zb-zt)/100, 2))
-        xa= xb
-        ya= yb
-        za= zb
-        volume= getVolume(xb, yb, zb)
-        if volume == "Exit":
-          stay= False
-          print(volume)
+  ptof[m][0]= round( pto[0][0]/100 ,2) 
+  ptof[m][1]= round( pto[0][1]/100 ,2)
+  ptof[m][2]= round( pto[0][2]/100 ,2)
+
+
+#Apply return
+
+print(disf)
+print(ptof)
