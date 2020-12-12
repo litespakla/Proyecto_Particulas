bc389a3d6e32a8a0ad4a81df1a74022ffb34ca19
Sebastianm1996 <57931205+Sebastianm1996@users.noreply.github.com>
2020-12-11 21:05:44 -0600

    Update coeficientes arreglado.py

--- coeficientes arreglado.py
+++ coeficientes arreglado.py
@@ -26,20 +26,10 @@ def changevalue(energia):
     absmusculo = 3773.19217*energia**(-2.69091704)
     totmusculo = 3774.03737*energia**(-2.68821618)
     abspiel = 3215.71787*energia**(-2.73458938)    
-    
-  
-    if energia <= 20:
-        totpiel = 3580.323*energia**(-2.73456854)
-        
-    else:
-        totpiel = 1.47172815*energia**(-2.74523673)
+    totpiel = 3580.323*energia**(-2.73456854)
+    totgrasa = 2695.2778397*energia**(-2.74969947)
         
 
-    if energia <= 20:
-        totgrasa = 2695.2778397*energia**(-2.74969947)
-        
-    else:
-        totgrasa = 1.06417447*energia**(-0.39794441)
         
    
     if energia <= 4:
